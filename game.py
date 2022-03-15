8a10050ef01ddce78c488b778ace53cc525b95af
Roland Sobczak <rolandsobczak@gamil.com>
2022-03-15 20:24:47 +0100

    game module refractor and some new tests

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ Raises:
     CrupierWins: when user exceeds 21 points
 """
 from core.players import User, Croupier
-from core.exceptions import InvalidInput, Draw, CrupierWins
+from core.exceptions import InvalidInput, Draw, CrupierWins, UserWins
 
 
 class Game:
@@ -29,8 +29,84 @@ class Game:
         self.user.passed = True
         print(f'Croupier cards: {"; ".join([str(card) for card in self.croupier.cards])}')
 
+    @property
+    def stats(self) -> str:
+        """Generates output for user
+
+        Returns:
+            str: Output for user
+        """
+        return f'Your cards: {"; ".join([str(card) for card in self.user.cards])}\n' \
+        f'Your score: {self.user.score}\n' \
+        f'Croupiers first card: {self.croupier.cards[0]}'
+
+
+    def _user_turn(self):
+        """Called when user have to make choice
+
+        Raises:
+            InvalidInput: Raises when user gives invalid input
+        """
+        choices = {
+            '1': self.hit,
+            '2': self.user_passed
+        }
+        while not self.user.passed:
+            print(self.stats)
+            print('Your turn:\n\t(1) hit\n\t(2) pass')
+            choice = input('Your choice?(1/2): ')
+
+            if (action := choices.get(choice)):
+                action()
+            else:
+                raise InvalidInput('Input is incorrect!'
+                                   ' Please choose one of the available option')
+
+
+    def _croupier_turn(self):
+        """This method is responsible for croupier choices
+        """
+        while self.croupier.score < self.user.score:
+            self.croupier.add_cards(self.croupier.deck.card_distribution())
+            print(f'Croupier cards: {"; ".join([str(card) for card in self.croupier.cards])}')
+
+
+    def _check_result(self):
+        """Raises correct exception in case when no players exceed max score"""
+        if self.croupier.score == self.user.score:
+            raise Draw('Draw')
+
+        if self.croupier.score > self.user.score and self.croupier.score <= 21:
+            raise CrupierWins('You lose')
+
 
     def game_start(self):
+        """Called when game's reviving
+        """
+        try:
+            self._user_turn()
+        except InvalidInput as exception:
+            print(exception)
+            self.game_start()
+
+        self._croupier_turn()
+        self._check_result()
+
+
+    def _setup(self) -> list:
+        """Setups initial game state
+
+        Returns:
+            list: croupiers deck
+        """
+        self.user.cards.clear()
+        self.croupier.cards.clear()
+        self.croupier.deck.setup()
+        self.user.add_cards(*self.croupier.game_init())
+        return self.croupier.deck
+
+
+    def new_game_start(self):
         """This method is the main part of the game
 
         Raises:
@@ -38,42 +114,22 @@ class Game:
             Draw: when user and croupier haves the same score
             CrupierWins: when user exceeds 21 points
         """
-        choices = {
-            '1': self.hit,
-            '2': self.user_passed
-        }
-        self.user.cards.clear()
-        self.croupier.cards.clear()
-        self.croupier.deck.setup()
-        self.user.add_cards(*self.croupier.game_init())
+        self._setup()
         print('New game!')
         try:
-            while not self.user.passed:
-                print(f'Your cards: {"; ".join([str(card) for card in self.user.cards])}')
-                print(f'Your score: {self.user.score}')
-                print(f'Croupiers first card: {self.croupier.cards[0]}')
-                print('Your turn:\n\t(1) hit\n\t(2) pass')
-                choice = input('Your choice?(1/2): ')
+            self.game_start()
 
-                if (action := choices.get(choice)):
-                    action()
-                else:
-                    raise InvalidInput('Input is incorrect!'
-                                       ' Please choose one of the available option')
-
-            while self.croupier.score < self.user.score:
-                self.croupier.add_cards(self.croupier.deck.card_distribution())
-                print(f'Croupier cards: {"; ".join([str(card) for card in self.croupier.cards])}')
-
-            if self.croupier.score == self.user.score:
-                raise Draw('Draw')
-
-            if self.croupier.score > self.user.score and self.croupier.score <= 21:
-                raise CrupierWins('You lose')
-
-
-        except Exception as exception:
+        except Draw as exception:
             print(exception)
 
+        except CrupierWins as exception:
+            print(exception)
+
+        except UserWins as exception:
+            print(exception)
+
+        except KeyboardInterrupt:
+            print('\nEnd\nthanks for game')
+
 if __name__ == '__main__':
-    Game().game_start()
+    Game().new_game_start()
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,8 @@
 """This module tests game module"""
 from game import Game
+from pytest import raises
+from core.exceptions import UserWins, CrupierWins, Draw
+from core.storage import Card
 
 
 def test_user_passed():
@@ -14,3 +17,45 @@ def test_hit():
     game = Game()
     game.hit()
     assert len(game.user.cards) == 1
+
+
+def test_croupier_hit():
+    """Checks if croupier plays until haves bigger score than user"""
+    with raises(UserWins) as message:
+        game = Game()
+        game._setup()
+        game.user.score = 22
+        game._croupier_turn()
+        assert message != 'You wins'
+
+def test_user_hit():
+    """Checks if CrupierWins exception is called when user exceed 21 points"""
+    with raises(CrupierWins) as message:
+        game = Game()
+        game.user.add_cards(Card('Clubs', 1), Card('Clubs', 1))
+        game.hit()
+        assert message != 'You lose!'
+
+def test_check_result():
+    """Checks if correct Exceptions are called when result is Draw or Lose"""
+    with raises(Draw) as message:
+        game = Game()
+        game.user.add_cards(Card('Clubs', 1), Card('Clubs', 1))
+        game.croupier.add_cards(Card('Clubs', 1), Card('Clubs', 1))
+        game._check_result()
+        assert message != 'Draw'
+
+    with raises(CrupierWins) as message:
+        game = Game()
+        game.user.add_cards(Card('Clubs', 1), Card('Clubs', 2))
+        game.croupier.add_cards(Card('Clubs', 1), Card('Clubs', 1))
+        game._check_result()
+        assert message != 'You lose!'
+
+def test_setup():
+    """Checks if user and croupier haves 2 cards after setup"""
+    game = Game()
+    game._setup()
+    assert len(game.user.cards) == 2
+    assert len(game.croupier.cards) == 2
+    assert set(game.user.cards) != set(game.croupier.cards)
